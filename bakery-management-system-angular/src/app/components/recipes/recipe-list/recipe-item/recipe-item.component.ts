97f65e1e6247054fefeab99d6f0d0dd10bc5cdda
unknown <alin.gurau@emanuel.ro>
2020-03-27 01:42:01 -0700

    added modifications to recipe-item

--- bakery-management-system-angular/src/app/components/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ bakery-management-system-angular/src/app/components/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,27 +1,30 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output } from '@angular/core';
 import { Recipe } from 'src/app/models/recipe.model';
 import { RecipeService } from 'src/app/services/recipe.service';
 import { ActivatedRoute, Router, Params } from '@angular/router';
 import { Observable } from 'rxjs';
+import { SelfUnsubscribe } from 'src/app/shared/self-unsubscribe';
 
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
   styleUrls: ['./recipe-item.component.scss']
 })
-export class RecipeItemComponent implements OnInit {
+export class RecipeItemComponent extends SelfUnsubscribe implements OnInit {
 
-  recipet: Observable<Recipe>;
-  id: number;
+  // recipe: Observable<Recipe>;
+  // id: number;
 
-  @Input() recipe: Recipe;
-  @Input() index: number;
+  @Input() recipe: Observable<Recipe>;
+  @Input() id: number;
 
   constructor(
     private recipeService: RecipeService,
     private route: ActivatedRoute,
     private router: Router
-  ) { }
+  ) {
+    super();
+   }
 
   ngOnInit() {
 
@@ -29,7 +32,7 @@ export class RecipeItemComponent implements OnInit {
     .subscribe(
       (params: Params) => {
         this.id = +params['id'];
-        this.recipet = this.recipeService.getRecipe(this.id);
+        this.recipe = this.recipeService.getRecipe(this.id);
       }
     );
   }
